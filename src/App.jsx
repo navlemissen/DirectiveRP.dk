62229fd72577e0a61c154d0c327e14d4c3814008
Navle <noahfredsted@gmail.com>
2025-12-17 16:39:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className="text-center text-white">Hello World</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
